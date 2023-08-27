93e147537291674fb57f591049708e693e5cb3bd
my_git <@89872336622@yandex.ru>
2023-08-27 21:52:11 +0300

    сделал ридми

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
+        Log::info('Запускаем команду user:check_expiratio');
         $schedule->command('user:check_expiration')->everyMinute();
     }
 
